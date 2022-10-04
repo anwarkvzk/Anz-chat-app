4f9f23e18e3e80ed8e53f9c7ca4b954945495332
anwarkvzk <anvarvzk521@gmail.com>
2022-10-04 14:25:21 +0530

    Backend Api Error Handlers

--- server.js
+++ server.js
@@ -2,20 +2,24 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./backend/data/data");
 const connectDB = require("./backend/config/db");
-const colors = require('colors')
-const userRoutes = require('./backend/routes/userRoutes')
+const colors = require("colors");
+const userRoutes = require("./backend/routes/userRoutes");
+const {notFound, errorHandler} = require('./backend/middleware/errorMiddleware')
 
 dotenv.config();
-connectDB()
+connectDB();
 const app = express();
 
-app.use(express.json()) //to accept JSON Data
+app.use(express.json()); //to accept JSON Data
 
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-app.use('/api/user', userRoutes)
+app.use("/api/user", userRoutes);
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
