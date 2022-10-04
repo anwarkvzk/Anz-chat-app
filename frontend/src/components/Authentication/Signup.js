d48dd307f48f242d933480ba2f0d07e70dfbd71f
anwarkvzk <anvarvzk521@gmail.com>
2022-10-04 19:23:03 +0530

    Frontend Signup continued

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -7,8 +7,11 @@ import {
   InputRightElement,
   useToast,
   VStack,
+
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import  { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function Signup() {
   const [show, setShow] = useState(false);
@@ -19,6 +22,7 @@ function Signup() {
   const [pic, setPic] = useState();
   const [loading, setLoading] = useState(false);
   const toast = useToast();
+  const history = useHistory();
 
   const handleClick = () => setShow(!show);
   const postDetails = (pics) => {
@@ -65,7 +69,66 @@ function Signup() {
       return;
     }
   };
-  const submitHandler = () => {};
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!name || !email || !password || !confirmPassword) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+    if (password !== confirmPassword) {
+      toast({
+        title: "Passwords Do Not Match",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user",
+        { name, email, password, pic },
+        config
+      );
+        console.log(data)
+      toast({
+        title: "Registration Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: " Eroor Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false)
+    }
+  };
 
   return (
     <VStack spacing="5px" color="black">
