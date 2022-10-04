40514b14f8fed3669e59d765ec75d8d829ae66df
anwarkvzk <anvarvzk521@gmail.com>
2022-10-04 11:58:13 +0530

    Backend routes Registrtion API

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./backend/data/data");
 const connectDB = require("./backend/config/db");
 const colors = require('colors')
+const userRoutes = require('./backend/routes/userRoutes')
 
 const app = express();
 dotenv.config();
@@ -12,15 +13,7 @@ app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  // console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use('/api/user', userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
