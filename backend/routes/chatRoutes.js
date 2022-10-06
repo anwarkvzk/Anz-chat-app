1d0af6f4dbe74afd89c1452ff5fd4ffc53e5405d
anwarkvzk <anvarvzk521@gmail.com>
2022-10-06 10:35:15 +0530

    Crete One on One Chat Api created

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const { accessChat } = require('../controllers/chatConteollers');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
-// router.route('/').post(protect , accessChat)
+router.route('/').post(protect , accessChat)
 // router.route("/").get(protect, fetchChats)
 // router.route('/group').post(protect, createGroupChat)
 // router.route("/rename").put(protect, renameGroup)
 // router.route("groupremove").put(protect, removeFromGroup)
 // router.route("/groupadd").put(protect, addToGroup)
 
-module.export = router
+module.exports = router 
