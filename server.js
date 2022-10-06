b29cf1669e06204fab0acc402f46e0a564988461
anwarkvzk <anvarvzk521@gmail.com>
2022-10-06 09:14:54 +0530

    Definig ChatRoutes Api

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const { chats } = require("./backend/data/data");
 const connectDB = require("./backend/config/db");
 const colors = require("colors");
 const userRoutes = require("./backend/routes/userRoutes");
-const {notFound, errorHandler} = require('./backend/middleware/errorMiddleware')
+const chatRoutes = require("./backend/routes/chatRoutes")
+const {
+  notFound,
+  errorHandler,
+} = require("./backend/middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -17,9 +21,10 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
