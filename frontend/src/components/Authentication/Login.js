7c865f006722d56947887e6c47fb179faddb4e11
anwarkvzk <anvarvzk521@gmail.com>
2022-10-06 08:57:04 +0530

    Authorization middleware token Added

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -64,7 +64,7 @@ function Login() {
     } catch (error) {
       toast({
         title: "Error Occured",
-        description:error.response.data.message,
+        description: error.response.data.message,
         status: "error",
         duration: 5000,
         isClosable: true,
